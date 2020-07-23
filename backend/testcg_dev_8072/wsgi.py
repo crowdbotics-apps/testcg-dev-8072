54e6e21ff0453e7be1588f51459f678b598d9325
Crowdbotics <team@crowdbotics.com>
2020-07-23 19:02:47 +0000

    Initial commit.

---
+++ backend/testcg_dev_8072/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testcg_dev_8072 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testcg_dev_8072.settings')
+
+application = get_wsgi_application()
